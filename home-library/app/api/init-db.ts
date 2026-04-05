f55a035d0e855ea6158e6d399f7f47b9d0aa9ab5
Лещев Дмитрий <waffledoto@gmail.com>
2026-04-05 20:21:25 +0300

    Update init-db.ts

--- home-library/app/api/init-db.ts
+++ home-library/app/api/init-db.ts
@@ -1,8 +1,1 @@
-import { initDb } from '@/lib/db';
 
-// Инициализация БД при запуске сервера
-if (process.env.NODE_ENV !== 'production') {
-  initDb().catch(console.error);
-}
-
-export { initDb };
